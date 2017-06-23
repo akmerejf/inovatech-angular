b4027913ae3663a885146f524f1c80cfedda0b46
SavioGuedes <savioguedess@gmail.com>
2017-06-23 15:59:04 -0300

    tomate

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -10,6 +10,14 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+  	$('.carousel.carousel-slider').carousel({fullWidth: true},setTimeout(autoplay));
+  
+  	function autoplay() {
+  		$('.carousel').carousel('next');
+  		setTimeout(autoplay, 5000);
+  	}
+	
   }
 
 }
