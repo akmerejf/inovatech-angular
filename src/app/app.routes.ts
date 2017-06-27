555e0001f9119892d33f422e4d7c3a5beecb381e
bitlucas <lucasbmat@gmail.com>
2017-06-27 10:30:35 -0300

    Footer mais bonitinho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {MainComponent} from './main/main.component';
 import {LoginComponent} from './login/login.component';
 import {CadastroComponent} from './cadastro/cadastro.component';
+import {ProjectComponent} from './projetos/project.component';
 
 const appRoutes: Routes = [
 	{path: '', component: MainComponent},
 	{path: 'login', component: LoginComponent},
-	{path: 'cadastro', component: CadastroComponent}
+	{path: 'cadastro', component: CadastroComponent},
+	{path: 'projetos', component: ProjectComponent}
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
